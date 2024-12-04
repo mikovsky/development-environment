496536221511aa43c61c6a4faf82d17dd211998a
mikovsky <dudek.michal1996@gmail.com>
2024-12-04 22:43:02 +0100

    enable catppuccin in lualine, neotree, and mason

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,13 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	priority = 1000
+	priority = 1000,
+  config = function ()
+    require("catppuccin").setup({
+      integrations = {
+        neotree = true,
+        mason = true,
+      }
+    })
+  end
 }
