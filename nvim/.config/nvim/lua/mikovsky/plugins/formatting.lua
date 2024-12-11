fd55d2c43eb36db774406adf73bbfbc33f1929fd
mikovsky <dudek.michal1996@gmail.com>
2024-12-12 00:53:39 +0100

    change formatting keymap

--- nvim/.config/nvim/lua/mikovsky/plugins/formatting.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/formatting.lua
@@ -9,6 +9,6 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<leader>mp", vim.lsp.buf.format, { desc = "Format Code using LSP" })
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format Code using LSP" })
 	end,
 }
