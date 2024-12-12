62ce5030c719cb92953de350e8ac908b0df9039b
Michal Dudek <michal.dudek@zowie.ai>
2024-12-12 12:10:44 +0100

    telescope - ignore venv directory

--- nvim/.config/nvim/lua/mikovsky/plugins/telescope.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/telescope.lua
@@ -26,6 +26,7 @@ return {
                         "target/",
                         ".idea/",
                         ".vscode/",
+                        "venv/",
                     },
                     path_display = { "smart" },
                     mappings = {
