87d3417b2cbfcc9b3807fbba6d61d09f3962d76b
mikovsky <dudek.michal1996@gmail.com>
2024-12-16 18:49:00 +0100

    options - reorganize options

--- nvim/.config/nvim/lua/mikovsky/core/options.lua
+++ nvim/.config/nvim/lua/mikovsky/core/options.lua
@@ -1,52 +1,46 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
--- basics
-vim.opt.relativenumber = true
+-- Left Column & similar settings
 vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 2
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
 vim.opt.scrolloff = 10
-vim.opt.showmode = false
-vim.opt.mouse = "a"
+vim.opt.sidescrolloff = 8
 
--- tabs & indentation
+-- Tabs & Spacing
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
 
--- wrapping
-vim.opt.wrap = false
-
--- searching
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-
--- current line
-vim.opt.cursorline = true
-
--- colors
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.opt.signcolumn = "yes"
-
--- backspace behavior
-vim.opt.backspace = "indent,eol,start"
-
--- clipboard config
-vim.opt.clipboard:append("unnamedplus")
-
--- splits
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- infrastructure
-vim.opt.updatetime = 50
-vim.opt.timeoutlen = 300
+-- General Behaviors
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 1000
+vim.opt.writebackup = false
+vim.opt.cursorline = true
+vim.opt.background = "dark"
+vim.opt.backspace = "indent,eol,start"
+
+-- Searching Behavior
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
 
 -- enable highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
