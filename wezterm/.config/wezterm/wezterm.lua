06d73f1ac614bfa6d8d0b80fba15b0fcc733bb88
mikovsky <dudek.michal1996@gmail.com>
2024-12-19 19:27:02 +0100

    wezterm - reformat config code

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,14 +2,18 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-	enable_wayland = false,
-	automatically_reload_config = true,
-	enable_tab_bar = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
-	color_scheme = "Tokyo Night",
-	font_size = 12,
-	font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+    enable_wayland = false,
+    automatically_reload_config = true,
+    enable_tab_bar = false,
+    window_close_confirmation = "NeverPrompt",
+    window_decorations = "RESIZE",
+    color_scheme = "Tokyo Night",
+    font_size = 12,
+    font = wezterm.font("MesloLGS Nerd Font Mono", {
+        weight = "Regular",
+        stretch = "Normal",
+        style = "Normal",
+    }),
 }
 
 return config
