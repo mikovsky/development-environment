06f677263b266aee11d0bf0e7dcee8db81c1a268
Michal Dudek <michal.dudek@zowie.ai>
2025-01-07 11:26:12 +0100

    disable alpha plugin, remove padding from wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,6 +14,12 @@ config = {
         stretch = "Normal",
         style = "Normal",
     }),
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    }
 }
 
 return config
