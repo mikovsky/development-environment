4dca81ce59c756e2d64da4ba096b0f7c5face4be
Michal Dudek <michal.dudek@zowie.ai>
2025-01-07 11:35:40 +0100

    remove padding, move bar to bottom

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,12 +14,6 @@ config = {
         stretch = "Normal",
         style = "Normal",
     }),
-    window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
-    }
 }
 
 return config
