30711f19a11088777e40547714596575f3dc3df7
Michal Dudek <michal.dudek@zowie.ai>
2025-01-09 12:13:12 +0100

    wezterm - add jetbrains font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,19 +1,20 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+---@diagnostic disable-next-line: unused-local
+local jetbrains_font = "JetBrains Mono"
+---@diagnostic disable-next-line: unused-local
+local meslo_font = "MesloLGS Nerd Font Mono"
+
 config = {
-    enable_wayland = false,
-    automatically_reload_config = true,
-    enable_tab_bar = false,
-    window_close_confirmation = "NeverPrompt",
-    window_decorations = "TITLE | RESIZE",
-    color_scheme = "Tokyo Night",
-    font_size = 12,
-    font = wezterm.font("MesloLGS Nerd Font Mono", {
-        weight = "Regular",
-        stretch = "Normal",
-        style = "Normal",
-    }),
+	enable_wayland = false,
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "TITLE | RESIZE",
+	color_scheme = "Tokyo Night",
+	font_size = 12,
+	font = wezterm.font(jetbrains_font, { weight = "Regular" }),
 }
 
 return config
