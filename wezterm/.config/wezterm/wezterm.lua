2728e946c1940b004e22f77e1b6edb5703f141a3
Michal Dudek <michal.dudek@zowie.ai>
2025-01-10 11:31:09 +0100

    wezterm - disable ligatures

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ local meslo_font = "MesloLGS Nerd Font Mono"
 config = {
 	enable_wayland = false,
 	automatically_reload_config = true,
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	enable_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "TITLE | RESIZE",
