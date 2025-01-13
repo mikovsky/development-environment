e00e17c48bddf12a0c0d7e2455dd99daa583b731
mikovsky <dudek.michal1996@gmail.com>
2025-01-13 18:34:02 +0100

    trouble - reformat file

--- nvim/.config/nvim/lua/mikovsky/plugins/trouble.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/trouble.lua
@@ -17,7 +17,7 @@ return {
       desc = "Open trouble document diagnostics",
     },
     { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<CR>",  desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>Trouble todo toggle<CR>",     desc = "Open todos in trouble" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+    { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
   },
 }
