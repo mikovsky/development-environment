65141900a43f6de73e4f7f551aeb02d9b54adb4c
mikovsky <dudek.michal1996@gmail.com>
2025-01-13 17:40:21 +0100

    migrate to blink.cmp and reorganize config

--- nvim/.config/nvim/lua/mikovsky/plugins/formatting.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/formatting.lua
@@ -1,26 +1,40 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      -- lua = { "stylua" },
-      python = { "black", "isort" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      css = { "prettier" },
-      html = { "prettier" },
-      json = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "black", "isort" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      notify_on_error = true,
     },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    notify_on_error = true,
+    init = function()
+      vim.keymap.set("n", "<leader>cf", function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end, { desc = "Format Code" })
+    end,
   },
-  init = function()
-    vim.keymap.set("n", "<leader>cf", function()
-      require("conform").format({ async = true, lsp_fallback = true })
-    end, { desc = "Format Code" })
-  end,
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    config = function()
+      require("ibl").setup({
+        scope = {
+          enabled = false,
+        },
+      })
+    end,
+  },
 }
