8e610f7d67eb24ed4ac44b3148fad882bf3c8f88
mikovsky <dudek.michal1996@gmail.com>
2025-01-14 22:01:53 +0100

    colorscheme - disable italics in tokyonight

--- nvim/.config/nvim/lua/mikovsky/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/colorscheme.lua
@@ -4,6 +4,17 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("tokyonight").setup({
+        styles = {
+          comments = {
+            italic = false,
+          },
+          keywords = {
+            italic = false,
+          },
+        },
+      })
       vim.cmd.colorscheme("tokyonight-night")
     end,
   },
