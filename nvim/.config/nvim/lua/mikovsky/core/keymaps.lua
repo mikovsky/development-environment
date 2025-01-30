7642d2f155dfcf2617037c2769734e44fe8224d8
mikovsky <dudek.michal1996@gmail.com>
2025-01-30 22:30:58 +0100

    tmux-sessionizer: improve setup

--- nvim/.config/nvim/lua/mikovsky/core/keymaps.lua
+++ nvim/.config/nvim/lua/mikovsky/core/keymaps.lua
@@ -37,6 +37,7 @@ vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase window he
 vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- tab management
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
