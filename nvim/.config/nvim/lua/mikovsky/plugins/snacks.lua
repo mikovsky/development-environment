853d541608f68ac50f689d143a93cb591e53b45f
Michal Dudek <michal.dudek@zowie.ai>
2025-02-04 16:08:43 +0100

    neotree: go back to neotree from snacks

--- nvim/.config/nvim/lua/mikovsky/plugins/snacks.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/snacks.lua
@@ -2,22 +2,12 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      explorer = {},
-      picker = { sources = { explorer = { hidden = true } } },
       lazygit = {},
       indent = { animate = { enabled = false }, scope = { enabled = false } },
     },
     config = function(_, opts)
       local snacks = require("snacks")
       snacks.setup(opts)
-
-      vim.keymap.set("n", "<leader>e", function()
-        snacks.explorer.open({ follow_file = false })
-      end, { desc = "Open Explorer" })
-
-      vim.keymap.set("n", "<leader>fe", function()
-        snacks.explorer.open({ follow_file = true })
-      end, { desc = "Open Explorer at current file" })
 
       vim.keymap.set("n", "<leader>gg", function()
         snacks.lazygit.open()
