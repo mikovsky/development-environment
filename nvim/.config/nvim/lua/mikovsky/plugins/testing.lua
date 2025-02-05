b327c89e97bf49f20754168eb03899c7258502ec
mikovsky <dudek.michal1996@gmail.com>
2025-02-05 17:21:20 +0100

    testing: remove comments

--- nvim/.config/nvim/lua/mikovsky/plugins/testing.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/testing.lua
@@ -55,15 +55,11 @@ return {
       "nvim-lua/plenary.nvim",
     },
     config = function()
-      -- Używamy bloop do testów
       -- vim.g['test#scala#runner'] = 'blooptest'
-      -- lub dla sbt:
       vim.g["test#scala#runner"] = "sbttest"
 
-      -- Strategie uruchamiania testów
       vim.g["test#strategy"] = "neovim"
 
-      -- Mapowania klawiszy
       vim.keymap.set("n", "<leader>vtn", ":TestNearest<CR>", { desc = "[T]ests: Run Nearest Test" })
       vim.keymap.set("n", "<leader>vtr", ":TestFile<CR>", { desc = "[T]ests: Run Test File" })
       vim.keymap.set("n", "<leader>vts", ":TestSuite<CR>", { desc = "[T]ests: Run Test Suite" })
