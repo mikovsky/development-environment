34adb60b7d47f0f886971e124655b54c99c1051e
Michal Dudek <michal.dudek@zowie.ai>
2025-02-06 12:34:42 +0100

    git: bring back lazygit

--- nvim/.config/nvim/lua/mikovsky/plugins/git.lua
+++ nvim/.config/nvim/lua/mikovsky/plugins/git.lua
@@ -46,4 +46,20 @@ return {
       end,
     },
   },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    },
+  },
 }
