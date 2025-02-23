36a90bff417fb381019d1b806cee114a4c0a40dd
mikovsky <dudek.michal1996@gmail.com>
2025-02-23 23:22:21 +0100

    add harpoon and lualine

---
+++ nvim2/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,27 @@
+local function get_total_lines_and_characters_in_line()
+  local characters = tostring(vim.fn.col("$") - 1)
+  return "%L | " .. characters
+end
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          icons_enabled = true,
+          theme = "tokyonight",
+          disabled_filetypes = {
+            statusline = {
+              "neo-tree",
+            },
+          },
+        },
+        sections = {
+          lualine_z = { "location", get_total_lines_and_characters_in_line },
+        },
+      })
+    end,
+  },
+}
