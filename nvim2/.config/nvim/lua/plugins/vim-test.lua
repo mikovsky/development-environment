6f329cc706202b257638f77cbe707def52380cb6
mikovsky <dudek.michal1996@gmail.com>
2025-02-24 19:57:00 +0100

    update vim-test keymaps

--- nvim2/.config/nvim/lua/plugins/vim-test.lua
+++ nvim2/.config/nvim/lua/plugins/vim-test.lua
@@ -7,10 +7,10 @@ return {
     vim.g["test#scala#runner"] = "sbttest" -- "blooptest"
     vim.g["test#strategy"] = "neovim"
 
-    vim.keymap.set("n", "<leader>vtn", ":TestNearest<CR>", { desc = "[T]ests: Run Nearest Test" })
-    vim.keymap.set("n", "<leader>vtr", ":TestFile<CR>", { desc = "[T]ests: Run Test File" })
-    vim.keymap.set("n", "<leader>vts", ":TestSuite<CR>", { desc = "[T]ests: Run Test Suite" })
-    vim.keymap.set("n", "<leader>vtl", ":TestLast<CR>", { desc = "[T]ests: Run Last Test" })
-    vim.keymap.set("n", "<leader>vtv", ":TestVisit<CR>", { desc = "[T]ests: Visit Last Test" })
+    vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>", { desc = "[T]ests: Run Nearest Test" })
+    vim.keymap.set("n", "<leader>tr", ":TestFile<CR>", { desc = "[T]ests: Run Test File" })
+    vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>", { desc = "[T]ests: Run Test Suite" })
+    vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", { desc = "[T]ests: Run Last Test" })
+    vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>", { desc = "[T]ests: Visit Last Test" })
   end,
 }
