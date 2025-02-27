c2fb334fd49b4bf56bf109a4b94b9bd98a48aa0b
mikovsky <dudek.michal1996@gmail.com>
2025-02-27 14:42:19 +0100

    remove nvim2 config

--- nvim2/.config/nvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "github/copilot.vim",
-  config = function()
-    vim.keymap.set("n", "<leader>cpe", "<cmd>Copilot enable<CR>", { desc = "Copilot: Enable" })
-    vim.keymap.set("n", "<leader>cpd", "<cmd>Copilot disable<CR>", { desc = "Copilot: Disable" })
-  end,
-}
